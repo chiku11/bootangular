37ca02645f66b750b027d1f636a61c7f0bbac04a
vavisr01 <srikant.vavillapalli.ap@nielsen.com>
2017-11-15 00:09:51 -0600

    newline fix

--- src/app/pipes/prettyprint.pipe.ts
+++ src/app/pipes/prettyprint.pipe.ts
@@ -6,18 +6,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PrettyprintPipe implements PipeTransform {
 
   transform(value: JSON): string {
+    
     if(value == null)
     return;
-
-    let tokens = JSON.stringify(value).
-    replace(`{`,``)
-    .replace(`}`,``).replace(`":"`,` `).split(`,`);
+    
+    let newString =  this.replaceAll (this.replaceAll( this.replaceAll(JSON.stringify(value),`{`,``) , `}`, ` ` ) , `"`,``).split(`,`); 
     let modified ='<br/>';
-    tokens.forEach(token =>{
+    newString.forEach(token =>{
       modified = `${modified+token} <br/> `; 
     })
     console.log(modified);
     return modified;
   }
 
+
+  escapeRegExp(string):string{
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  }
+
+   replaceAll(str, term, replacement):string {
+    return str.replace(new RegExp(this.escapeRegExp(term), 'g'), replacement);
+  }
 }
